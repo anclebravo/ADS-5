9bb16354e134aaea3d976f8bb1d06bfd4e917b85
pereverzevalyuba <62954283+pereverzevalyuba@users.noreply.github.com>
2020-05-16 18:20:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -9,33 +9,26 @@ class TPQueue
         T data;
         ITEM* next;
     };
-
 public:
     TPQueue() :head(nullptr), tail(nullptr) {}
     ~TPQueue();
-
     void push(const T&);
     T pop();
-
     void print() const;
-
 private:
     TPQueue::ITEM* create(const T&);
     ITEM* head;
     ITEM* u;
     ITEM* tail;
 };
-
 template<typename T>
 typename TPQueue<T>::ITEM* TPQueue<T>::create(const T& data)
 {
     ITEM* item = new ITEM;
     item->data = data;
     item->next = nullptr;
-
     return item;
 }
-
 template<typename T>
 TPQueue<T>::~TPQueue()
 {
@@ -115,11 +108,9 @@ T TPQueue<T>::pop()
         T data = head->data;
         delete head;
         head = temp;
-
         return data;
     }
 }
-
 template<typename T>
 void TPQueue<T>::print() const	
 {
@@ -135,11 +126,9 @@ void TPQueue<T>::print() const
         std::cout << std::endl;	
     }
 }
-
-
 struct SYM
 {
 	char ch;
 	int  prior;
-	SYM* next;
 };
+SYM* next;
